38034e954985d2ca5eeb2f9dbf2b4b7ffca5eed0
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-08-11 18:25:13 +0800

    adjust tech components

--- src/components/TechHeading.jsx
+++ src/components/TechHeading.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function TechHeading({ heading }) {
-  return <h3 className="font-light uppercase">{heading}</h3>;
+  return (
+    <h3 className="font-light text-white uppercase md:text-xl">{heading}</h3>
+  );
 }
--- src/components/TechItem.jsx
+++ src/components/TechItem.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function TechItem({ techName = "", color = "" }) {
   return (
-    <div className="border-border-light flex items-center gap-2 rounded-md border-1 p-3 md:p-5">
+    <div className="border-border-light flex items-center gap-3 rounded-md border-1 p-3 md:gap-5 md:p-5">
       <div className={`h-8 w-8 rounded-md ${color}`}></div>
-      <p className="text-sm md:text-lg">{techName}</p>
+      <p className="text-tertiary-white text-sm md:text-lg">{techName}</p>
     </div>
   );
 }
