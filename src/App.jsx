a1b7fe40147637596830957ebc38e1a065f229be
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-08-13 20:40:29 +0800

    add new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About";
+import Projects from "./pages/Projects.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
         {/* project */}
         {/* actual project */}
         {/* resume */}
