55d413fa6cdc9dfafd976d6fa3f0983839da04cf
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-08-14 13:41:20 +0800

    add new component

---
+++ src/components/OtherProjectCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function OtherProjectCard() {
+  return (
+    <>
+      <div className="">
+        {/* image */}
+        <div className="">
+          <div className="h-48 rounded-lg bg-red-300"></div>
+        </div>
+        {/* text */}
+        <div className="mt-10 text-center capitalize">
+          <h3>Mobhie</h3>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 import Navigator from "@components/Navigator";
 import PinnedProjectCard from "../components/PinnedProjectCard";
+import SectionLine from "../components/SectionLine";
+import OtherProjectCard from "../components/OtherProjectCard";
 
 export default function Projects() {
   return (
     <>
       <Navigator />
 
+      {/* pinned projects */}
       <div className="container mx-auto mt-24 w-10/12 max-w-5xl">
         <h1 className="mb-16 max-w-lg text-center text-3xl leading-relaxed font-bold tracking-wide uppercase md:mx-auto md:w-10/12 md:text-2xl">
           Projects
@@ -20,6 +23,24 @@ export default function Projects() {
 
           {/* item */}
           <PinnedProjectCard />
+        </div>
+      </div>
+
+      <SectionLine />
+
+      {/* other projects */}
+
+      <div className="container mx-auto mt-24 w-10/12 max-w-5xl">
+        <h2 className="mb-16 max-w-lg text-center text-3xl leading-relaxed font-bold tracking-wide uppercase md:mx-auto md:w-10/12 md:text-2xl">
+          other Projects
+        </h2>
+
+        <div className="flex flex-col justify-center gap-20">
+          {/* card */}
+          <OtherProjectCard />
+
+          {/* card */}
+          <OtherProjectCard />
         </div>
       </div>
 
