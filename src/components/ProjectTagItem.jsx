0b9dd939b68b2c9d5524724a967c3343b7b95d52
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-08-14 15:15:00 +0800

    add new component

---
+++ src/components/ProjectTagItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProjectTagItem() {
+  return (
+    <div className="bg-third-black text-tertiary-white inline-block rounded-full px-2 py-1 text-xs tracking-wider">
+      <p>voice-assistance</p>
+    </div>
+  );
+}
