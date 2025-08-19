1a8a1535f120eaf211f7def2ed3445b37fe043bb
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-08-19 19:29:09 +0800

    add data for services and technology section

--- src/components/TechItem.jsx
+++ src/components/TechItem.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-export default function TechItem({ techName = "", color = "" }) {
+export default function TechItem({ techName = "", icon, color }) {
   return (
-    <div className="border-border-light flex items-center gap-2 rounded-sm border-1 p-2 md:gap-5 md:p-3">
-      <div className={`h-8 w-8 rounded-md ${color}`}></div>
-      <p className="text-tertiary-white text-xs sm:text-sm md:text-lg">
+    <div className="border-border-light flex items-center gap-2 rounded-sm border-1 bg-white/5 p-2 md:gap-5 md:p-3">
+      <div
+        className={`h-10 w-10 rounded-md p-2`}
+        style={{ backgroundColor: `${color}20` }}
+      >
+        <div className="" dangerouslySetInnerHTML={{ __html: icon }}></div>
+      </div>
+
+      <p className="text-tertiary-white text-xs capitalize sm:text-sm md:text-lg">
         {techName}
       </p>
     </div>
