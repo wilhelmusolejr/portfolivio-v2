2ae4474b92addd19b40d8b9b4819839d738cab2f
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-08-20 14:38:46 +0800

    fix link issue when navigating to new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home.jsx";
 import About from "./pages/About";
 import Projects from "./pages/Projects.jsx";
 import Project from "./pages/Project.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/project/:name" element={<Project />} />
-        {/* actual project */}
-        {/* resume */}
       </Routes>
     </Router>
   );
