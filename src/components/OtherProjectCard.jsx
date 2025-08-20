e114d9fac2fc00a82481d43e3c2a7017835f6feb
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-08-20 14:02:32 +0800

    add lasy loading in images

--- src/components/OtherProjectCard.jsx
+++ src/components/OtherProjectCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LazyImage from "./LazyImage";
 
 export default function OtherProjectCard({ data }) {
   const project_image = `/assets/${data.project_showcase.url}${data.project_showcase.is_other.image}`;
@@ -7,11 +8,11 @@ export default function OtherProjectCard({ data }) {
 
   return (
     <>
-      <a href={`/project/${url_name}`} className="">
-        <div className="h-48 overflow-hidden">
-          <img
+      <a href={`/project/${url_name}`} className="w-full md:w-5/12 lg:w-3/12">
+        <div className="relative h-48 overflow-hidden">
+          <LazyImage
             src={project_image}
-            alt="Logo"
+            alt={data.name}
             className="mx-auto h-full w-auto rounded-lg object-cover"
           />
         </div>
