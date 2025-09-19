dcc0b2432c29785d94bd90a1e0b4a31c5caff2b8
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-09-19 16:57:09 +0800

    add animation on navigator when opening and closing the menu

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -6,9 +6,13 @@ import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 
 import NavigatorLink from "./NavigatorLink";
 
+import { motion, AnimatePresence } from "framer-motion";
+
 export default function Navigator() {
   const location = useLocation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const resume_link = `https://drive.google.com/file/d/1OYfzSr7biSVoiTU052svmt24gvcfev4p/view?usp=sharing`;
 
   function handleCloseMenu() {
     setIsMenuOpen(false);
@@ -61,34 +65,45 @@ export default function Navigator() {
           </div>
         </div>
       </nav>
-
-      {isMenuOpen && (
-        <div className="fixed inset-0 z-20 flex flex-col items-center justify-center gap-5 bg-black uppercase">
-          {/* about */}
-          <NavigatorLink CurrentPath={location.pathname} location="about" />
-
-          {/* project */}
-          <NavigatorLink CurrentPath={location.pathname} location="projects" />
-
-          <div className="bg-tertiary-white my-5 h-px w-7"></div>
-
-          {/* Resume */}
-          <a
-            href="https://drive.google.com/file/d/1OYfzSr7biSVoiTU052svmt24gvcfev4p/view?usp=sharing"
-            target="_blank"
-            className="transition-colors duration-200 hover:text-yellow-400 hover:underline"
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            className="fixed inset-0 z-20 flex flex-col items-center justify-center gap-5 bg-black uppercase"
+            initial={{ opacity: 0, y: -20 }} // when mounting
+            animate={{ opacity: 1, y: 0 }} // when open
+            exit={{ opacity: 0, y: -20 }} // when closing
+            transition={{ duration: 0.4, ease: "easeInOut" }}
           >
-            Resume
-          </a>
+            {/* about */}
+            <NavigatorLink CurrentPath={location.pathname} location="about" />
 
-          <div
-            className="absolute top-5 right-5 md:hidden"
-            onClick={handleCloseMenu}
-          >
-            <FontAwesomeIcon icon={faClose} className="text-2xl" />
-          </div>
-        </div>
-      )}
+            {/* project */}
+            <NavigatorLink
+              CurrentPath={location.pathname}
+              location="projects"
+            />
+
+            <div className="bg-tertiary-white my-5 h-px w-7"></div>
+
+            {/* Resume */}
+            <a
+              href={resume_link}
+              target="_blank"
+              className="transition-colors duration-200 hover:text-yellow-400 hover:underline"
+            >
+              Resume
+            </a>
+
+            {/* close button */}
+            <div
+              className="absolute top-5 right-5 md:hidden"
+              onClick={handleCloseMenu}
+            >
+              <FontAwesomeIcon icon={faClose} className="text-2xl" />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
